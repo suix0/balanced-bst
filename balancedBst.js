7bd937848ab3bb87b46b10e5cb510f739dc920df
Nethan Bagasbas <bagasbas.nethangabriel@gmail.com>
2024-09-14 01:46:26 +0000

    made buildTree function to convert an array to a bst

--- balancedBst.js
+++ balancedBst.js
@@ -1,8 +1,8 @@
 function Node() {
-  const root = null;
+  const data = null;
   const left = null;
   const right = null;
-  return { root, left, right };
+  return { data, left, right };
 }
 
 function removeDuplicates(arr) {
@@ -14,6 +14,18 @@ function removeDuplicates(arr) {
 }
 
 function Tree(array) {
+  function prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
   // root attribute
   let root;
 
@@ -23,8 +35,20 @@ function Tree(array) {
   const cleansedArr = removeDuplicates(sortedArr);
 
   function buildTree(arr, low, high) {
-    
+    if (low > high) {
+      return null;
+    }
+    const mid = Math.ceil((low + high) / 2);
+    let root = Node();
+    root.data = arr[mid];
+    root.left = buildTree(arr, low, mid - 1);
+    root.right = buildTree(arr, mid + 1, high);
+    return root;
   }
+
+  root = buildTree(cleansedArr, 0, cleansedArr.length - 1)
+  return { root, prettyPrint }
 } 
 
-// const bst = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+const bst = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+bst.prettyPrint(bst.root)
