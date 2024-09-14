a66c33fbc4032cd17ac78fd4cac09fda07aace77
Nethan Bagasbas <bagasbas.nethangabriel@gmail.com>
2024-09-14 01:37:08 +0000

    added remove duplicate function

--- balancedBst.js
+++ balancedBst.js
@@ -5,6 +5,26 @@ function Node() {
   return { root, left, right };
 }
 
-function Tree() {
-  const root = Node();
+function removeDuplicates(arr) {
+  const hash = {};
+  for (let i = 0; i < arr.length; i++) {
+    hash[arr[i]] = null;
+  }
+  return Object.keys(hash).map((key) => parseInt(key));
 }
+
+function Tree(array) {
+  // root attribute
+  let root;
+
+  // Sort the array
+  const sortedArr = array.sort((a, b) => a - b);
+  // Remove duplicates
+  const cleansedArr = removeDuplicates(sortedArr);
+
+  function buildTree(arr, low, high) {
+    
+  }
+} 
+
+// const bst = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
